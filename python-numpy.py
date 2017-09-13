b293f54f4132c57a725ef323907b647db4ef5d55
jkwanth <jkwanth@users.noreply.github.com>
2017-09-13 19:13:22 +0800

    Create python-numpy.py

---
+++ python-numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+np.asmatrix() #
+np.matrix() # 
+np.asarray()
+np.array()
+np.dot()  # (matrix, matrix) => multiplication result
+np.diag() # (vector) => diagonal matrix
+np.sqrt() # (scalar) => square root result
+np.identity()
+np.zeros()
+np.ones()
+np.append()
+
+np.random
+
+reshape
+size
+T
